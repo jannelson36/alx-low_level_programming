133af8c24ac576cd95e594957f84bcb29071ec6b
jannelson36 <jannelson36@gmail.com>
2022-09-13 10:16:42 +0300

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,28 +1,21 @@
-/*
- * File: 0-putchar.c
- * Auth: Jan Nelson
- */
-
 #include "main.h"
 
 /**
- * main - Print the word "_putchar"
- *
- * Return: Always 0.
+ * main - print _putchar, followed by new line
+ * Return: 0
  */
 
 int main(void)
 {
-	int i;
-
-	char p[] = "_putchar";
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(p[i]);
-	}
-	    _putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
-
 }
