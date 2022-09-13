dc5ea5142b9476fde12f69c5db3d82861533a826
jannelson36 <jannelson36@gmail.com>
2022-09-13 00:42:24 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,22 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Jan Nelson
+ *
+ */
+
 #include "main.h"
 
 /**
- * main - prints _putchar
+ * main - Prints "_putchar" followed by a new line.
  *
  * Return: Always 0.
-*/
-
+ */
 int main(void)
 {
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
-	_putchar('t');
+	_putchar('T');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
