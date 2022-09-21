5b6d537da04d4f4c7819bc3022ea481541932df3
jannelson36 <jannelson36@gmail.com>
2022-09-21 21:51:34 +0300

    debug errors

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,21 +15,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i, j;
 
 	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
 	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-		i++;
 		j++;
+		i++;
 	}
+
 	dest[i] = '\0';
+
 	return (dest);
 }
