d035e6f24b1faf2f5a5aa44432f97bda9c91fa01
jannelson36 <jannelson36@gmail.com>
2022-10-24 19:13:34 +0300

    linked lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+/*
+ * File: 1-list_len.c
+ * Auth: Jan Nelson
+ */
+
+#include "lists.h"
+
+/**
+ * list_len - Finds the number of elements in
+ *            a linked list_t list.
+ * @h: The linked list_t list.
+ *
+ * Return: The number of elements in h.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t e = 0;
+
+	while (h)
+	{
+		e++;
+		h = h->next;
+	}
+
+	return (e);
+}
