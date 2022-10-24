5cbe740bc59e5fd4cc3c8473a354b9d8977cf938
jannelson36 <jannelson36@gmail.com>
2022-10-24 19:25:24 +0300

    debugging error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,28 +3,22 @@
  * Auth: Jan Nelson
  */
 
-#include "lists.h"
 #include <stdio.h>
-
+#include "lists.h"
+#include <stdlib.h>
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: The list_t list.
- *
- * Return: The number of nodes in h.
+ * print_list - prints all elements of a list_t list
+ * @h: singly linked list to print
+ * Return: number of nodes in the list
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
-	while (h)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
+	size_t i;
 
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		nodes++;
+	for (i = 0; h; i++)
+	{
+		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 	}
-	return (nodes);
+	return (i);
 }
